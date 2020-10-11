c87deebe91717e1c7c793f2db01d4a1c8c543b13
곽경열 <62980973+kwakky1@users.noreply.github.com>
2020-10-12 02:15:32 +0900

    화면 결과값 출력 useEffect로 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Modal from "./components/Modal";
 
 const App = () => {
   const [handle, setHandle] = useState(false)
-  const [startSession] = useState(JSON.parse(sessionStorage.getItem('start')))
-  const [endSession] = useState(JSON.parse(sessionStorage.getItem('end')))
+  const startSession = JSON.parse(sessionStorage.getItem('start'))
+  const endSession = JSON.parse(sessionStorage.getItem('end'))
+
+  const [startResult, setStartResult] = useState('')
+  const [endResult, setEndResult] = useState('')
+
+  useEffect(()=>{
+    setStartResult(startSession)
+    setEndResult(endSession)
+  },[startSession, endSession])
 
   const openModal = () => {
     setHandle(!handle)
@@ -19,8 +27,8 @@ const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <button onClick={openModal}>모달나와라</button>
         <Modal handle={handle} setHandle={(value)=>(setHandle(value))}/>
-        <h1>{startSession}</h1>
-        <h1>{endSession}</h1>
+        <h1>{startResult}</h1>
+        <h1>{endResult}</h1>
       </header>
     </div>
   );
